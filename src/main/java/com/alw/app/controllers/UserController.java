d737977e96b5db59d8f6c6d6eb37fb4fbbf8c23f
kevin.gjika <gjikakevin@gmail.com>
2022-09-30 23:26:36 +0200

    mesazhi

--- src/main/java/com/alw/app/controllers/UserController.java
+++ src/main/java/com/alw/app/controllers/UserController.java
@@ -47,4 +47,6 @@ public class UserController {
         return new ResponseEntity("User deleted successfully.", HttpStatus.OK);
     }
 
+
+
 }
--- src/main/java/com/alw/app/repositories/UserRepository.java
+++ src/main/java/com/alw/app/repositories/UserRepository.java
@@ -2,11 +2,13 @@ package com.alw.app.repositories;
 
 import com.alw.app.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@Table
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByUsername (String message);
